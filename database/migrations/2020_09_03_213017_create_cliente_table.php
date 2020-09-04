ccce63c1baac4bfe12f50c14a2e61e482c5f37ed
Jose Diaz <joseangel191134@gmail.com>
2020-09-03 20:07:11 -0500

    add all to repository

---
+++ database/migrations/2020_09_03_213017_create_cliente_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClienteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('tipo_documento');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('direccion');
+            $table->string('ciudad');
+            $table->string('telefono');
+            $table->string('user_id'); 
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente');
+    }
+}
